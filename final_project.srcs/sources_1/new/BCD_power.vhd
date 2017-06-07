9e305c1f4f6561a1fac4ecca848ad18c9e5da34a
mma <mma137421@gmail.com>
2017-06-07 17:21:56 +0430

    Updates signature of power module

--- final_project.srcs/sources_1/new/BCD_power.vhd
+++ final_project.srcs/sources_1/new/BCD_power.vhd
@@ -34,7 +34,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity BCD_power is Port (
 base_BCD : in std_logic_vector( 31 downto 0);
 power_BCD : in std_logic_vector( 3 downto 0);
-result_BCD : out std_logic_vector( 63 downto 0 );
+result_BCD : out std_logic_vector( 127 downto 0 );
 AVF : out std_logic
 );
 end BCD_power;
